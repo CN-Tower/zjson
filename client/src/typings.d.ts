6fcf65379d91fa608e088558bc3cd4116db7bc86
CN-Tower <247114045@qq.com>
2018-05-13 14:21:00 +0800

    Create typings.d.ts

---
+++ client/src/typings.d.ts
@@ -0,0 +1,9 @@
+/* SystemJS module definition */
+declare var jQuery: any;
+declare var $: any;
+declare var fn: any;
+declare var window: Window;
+declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+}
