edbb767983f2e8d112a2fa70f91dffa8822a5e07
CN-Tower <cntower@yahoo.com>
2018-05-14 17:34:16 +0800

    Update docker.sh

--- client/build/docker.sh
+++ client/build/docker.sh
@@ -1,9 +1,7 @@
 #!/usr/bin
 
-version='V3.0.2'
-
 if [ $1 -eq 'run' ]; then
-    docker run --net=host -d pxjson:$version
+    docker run --net=host -d pxjson:V3.0.2
 else
-    docker build -t pxjson:$version .
+    docker build -t pxjson:V3.0.2 .
 fi
