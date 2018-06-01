456c24ade2ae8d5923b76bb61b240b78b33b0645
CN-Tower <cntower@yahoo.com>
2018-06-01 16:14:36 +0800

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var visitsWorker = require('../worker/visits-wroker');
+var visitsWork = require('../work/visits-count.wrok');
 
 router.get('/', function(req, res, next) {
   res.send('ok!');
 });
 
 router.get('/userId', function(req, res, next) {
-  res.send(200, {id: visitsWorker.addUser()});
+  res.send(200, {id: visitsWork.addUser()});
 });
 
 router.get('/visitCount/:userId', function(req, res, next) {
-  visitsWorker.getAndSetVisits(req.params['userId'], (visits, newUserId) => {
+  visitsWork.getAndSetVisits(req.params['userId'], (visits, newUserId) => {
     res.send(200, {nb: visits, id: newUserId || null});
   });
 });
