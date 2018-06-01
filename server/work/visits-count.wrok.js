94b76275c1a963efaf1771728b672ed365fc0e50
CN-Tower <cntower@yahoo.com>
2018-06-01 16:13:58 +0800

    Create visits-count.wrok.js

---
+++ server/work/visits-count.wrok.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var fn = require('funclib');
+var visits;
+var users = [];
+
+function getAndSetVisits(userId, onEnd) {
+    let user = users.filter(user => user.userId === userId);
+    if (!visits || user.length === 0 || fn.timeStamp() - user[0].vtTime > 300000) {
+        const newId = addUser();
+        doGetAndSet();
+        onEnd(visits, newId);
+    }
+    onEnd(visits, null);
+}
+
+function doGetAndSet() {
+    visits = parseInt(fs.readFileSync('./visit-count.txt', 'utf8'));
+    console.log('GET-visits: Current visits is: ' + visits);
+    fs.writeFileSync('./visit-count.txt', ++ visits);
+    console.log('SET-visits: Current visits now is: ' + visits);
+}
+
+function addUser() {
+    const userId = 'ZJSON-' + fn.gnid();
+    const vtTime = fn.timeStamp();
+    users.push({userId: userId, vtTime: vtTime});
+    setTimeout(function () {
+        users = users.filter(user => user.userId !== userId);
+    }, 300000);
+    return userId;
+}
+
+module.exports = {
+    getAndSetVisits: getAndSetVisits
+}
