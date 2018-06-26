81de9fbc7821ca38880e08ed23b1cc27aadd2434
CN-Tower <cntower@yahoo.com>
2018-06-26 08:42:35 +0800

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,24 +2,31 @@ var express = require('express');
 var router = express.Router();
 var fn = require('funclib');
 var vcWork = require('../work/visit-count.wrok');
-var vcService = require('../service/visit-count.service');
 
 router.get('/', function(req, res, next) {
   res.status(200).send('ok')
 });
 
 router.get('/zjson/version', function(req, res, next) {
-  res.status(200).send({'version': vcService.getVersion()});
+  res.status(200).send({'version': vcWork.getVersion()});
 });
 
-router.get('/vc/refreshVc/:userId', function(req, res, next) {
-  vcWork.refreshVisitCount(req.params['userId'], (userId) => {
+router.get('/online', function(req, res, next) {
+  res.status(200).send({'online': vcWork.getOnline()});
+});
+
+router.get('/user/:userId', function(req, res, next) {
+  res.status(200).send(vcWork.getUser(req.params['userId']) || {})
+});
+
+router.get('/refreshVc/:userId', function(req, res, next) {
+  vcWork.refreshVc(req.params['userId'], req.query['isExpire'], (userId) => {
     res.status(200).send({'id': userId});
   });
 });
 
-router.get('/vc/pollingVc/:userId', function(req, res, next) {
-  res.status(200).send({'vc': vcService.getVisitCount()});
+router.get('/pollingVc/:userId', function(req, res, next) {
+  res.status(200).send({'vc': vcWork.pollingVc(req.params['userId'])});
 });
-
+ 
 module.exports = router;
