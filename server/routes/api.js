045ebd15b13d10f54c892dcb704922aa0d7439fe
CN-Tower <cntower@yahoo.com>
2018-06-29 17:50:39 +0800

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,18 +1,40 @@
-var express = require('express');
-var router = express.Router();
-var fn = require('funclib');
-var vcWork = require('../work/visit-count.wrok');
+const express = require('express');
+const router = express.Router();
+const vcWork = require('../worker/visit-count.wroker');
+const VersionModel = require('../dao/models/version.model');
+const VcModel = require('../dao/models/visitCount.model');
+const UsersModel = require('../dao/models/users.model')
+const util = require('../tools/util');
+const fn = require('funclib');
 
 router.get('/', function(req, res, next) {
-  res.status(200).send('ok')
+  res.status(200).send('Api work!')
+});
+
+router.get('/zjson/users', function(req, res, next) {
+  UsersModel.getUsers((err, doc) => {
+    util.logErr(err, 'Get All visits Error');
+    res.status(200).send({'visits': doc});
+  });
 });
 
 router.get('/zjson/version', function(req, res, next) {
-  res.status(200).send({'version': vcWork.getVersion()});
+  VersionModel.getVersion((err, doc) => {
+    util.logErr(err, 'Get Version Error');
+    res.status(200).send({'version': doc.version});
+  });
+});
+
+router.post('/zjson/version', function(req, res, next) {
+  const version = req.body.version
+  VersionModel.setVersion(version, (err, doc) => {
+    util.logErr(err, 'Set Version Error');
+    res.status(200).send({version: version});
+  });
 });
 
 router.get('/online', function(req, res, next) {
-  res.status(200).send({'online': vcWork.getOnline()});
+  VcModel.getOnline(num => res.status(200).send({'online': num}));
 });
 
 router.get('/user/:userId', function(req, res, next) {
@@ -26,7 +48,9 @@ router.get('/refreshVc/:userId', function(req, res, next) {
 });
 
 router.get('/pollingVc/:userId', function(req, res, next) {
-  res.status(200).send({'vc': vcWork.pollingVc(req.params['userId'])});
+  vcWork.pollingVc(req.params['userId'], vc => {
+    res.status(200).send({'vc': vc});
+  });
 });
  
 module.exports = router;
