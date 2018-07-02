7f49aadf862c33efe30f73c089df5386e916065c
CN-Tower <cntower@yahoo.com>
2018-07-02 13:42:42 +0800

    Update visitCount.model.js

--- server/dao/models/visitCount.model.js
+++ server/dao/models/visitCount.model.js
@@ -4,14 +4,24 @@ const VisitCountModel = mongoose.model('version', VisitCountSchema);
 const util = require('../../tools/util');
 const fn = require('funclib');
 
-VisitCountModel.addOneVisit = function() {
-    this.findOneAndUpdate({"name": "zjson-version"}, {$inc: {count: 1}}, function(err, doc) {
-        util.logErr(err);
-    });
+VisitCountModel.addOneVisit = function(callback) {
+    this.findOneAndUpdate({"name": "zjson-version"}, {$inc: {count: 1}}, callback);
 }
 
 VisitCountModel.getVisitCount = function(callback) {
     return this.findOne({"name": "zjson-version"}, {}, callback);
+}
+
+VisitCountModel.setVisitCount = function(count, callback) {
+    this.getVisitCount((err, doc) => {
+        if (doc) {
+            this.findOneAndUpdate({"name": "zjson-version"}, {$set: {count: count}}, callback);
+        } else {
+            this.create(new VisitCountModel({
+                "name": "zjson-version", "count": count
+            }), callback);
+        }
+    });
 }
 
 module.exports = VisitCountModel;
