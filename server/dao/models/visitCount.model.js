9bba969d7d28f82a7c03124130c9722aec8e6f10
CN-Tower <247114045@qq.com>
2018-07-18 21:52:57 +0800

    Update

--- server/dao/models/visitCount.model.js
+++ server/dao/models/visitCount.model.js
@@ -5,20 +5,20 @@ const util = require('../../tools/util');
 const fn = require('funclib');
 
 VisitCountModel.addOneVisit = function(callback) {
-    this.findOneAndUpdate({"name": "zjson-version"}, {$inc: {count: 1}}, callback);
+    this.findOneAndUpdate({"name": "zjson-vc"}, {$inc: {count: 1}}, callback);
 }
 
 VisitCountModel.getVisitCount = function(callback) {
-    return this.findOne({"name": "zjson-version"}, {}, callback);
+    return this.findOne({"name": "zjson-vc"}, {}, callback);
 }
 
 VisitCountModel.setVisitCount = function(count, callback) {
     this.getVisitCount((err, doc) => {
         if (doc) {
-            this.findOneAndUpdate({"name": "zjson-version"}, {$set: {count: count}}, callback);
+            this.findOneAndUpdate({"name": "zjson-vc"}, {$set: {count: count}}, callback);
         } else {
             this.create(new VisitCountModel({
-                "name": "zjson-version", "count": count
+                "name": "zjson-vc", "count": count
             }), callback);
         }
     });
