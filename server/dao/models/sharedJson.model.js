42be82e31449523b1cc439da8c6cfe8f8140928a
CN-Tower <cntower@yahoo.com>
2018-08-24 15:45:32 +0800

    Create sharedJson.model.js

---
+++ server/dao/models/sharedJson.model.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const SharedJsonSchema = require('../schemas/sharedJson.schema');
+const SharedJsonModel = mongoose.model('shared-json', SharedJsonSchema);
+
+SharedJsonModel.getSjList = function(userId, callback) {
+  this.find({}, callback);
+}
+
+SharedJsonModel.getSjsByUserId = function(userId, callback) {
+  this.find({"userId": userId}, callback);
+}
+
+SharedJsonModel.getSjByUserId = function(userId, callback) {
+  this.findOne({"userId": userId}, callback);
+}
+
+SharedJsonModel.createSj = function(sharedJson, callback) {
+  this.create(new SharedJsonModel(sharedJson), callback);
+}
+
+SharedJsonModel.removeSjById = function(userId, callback) {
+  this.remove({"userId": userId}, callback);
+}
+
+SharedJsonModel.updateSjById = function(userId, data, callback) {
+  this.findOneAndUpdate({"userId": userId}, data, {"new": true}, callback);
+}
+
+module.exports = SharedJsonModel;
