5d1299c852248929f8be90398e7388df49bff83f
CN-Tower <cntower@yahoo.com>
2018-08-24 15:46:36 +0800

    Update visitCount.model.js

--- server/dao/models/visitCount.model.js
+++ server/dao/models/visitCount.model.js
@@ -1,25 +1,13 @@
 const mongoose = require('mongoose');
 const VisitCountSchema = require('../schemas/visitCount.schema');
-const VisitCountModel = mongoose.model('version', VisitCountSchema);
+const VisitCountModel = mongoose.model('visit-count', VisitCountSchema);
 
 VisitCountModel.addOneVisit = function(callback) {
     this.findOneAndUpdate({"name": "zjson-vc"}, {$inc: {count: 1}}, callback);
 }
 
 VisitCountModel.getVisitCount = function(callback) {
-    return this.findOne({"name": "zjson-vc"}, {}, callback);
-}
-
-VisitCountModel.setVisitCount = function(count, callback) {
-    this.getVisitCount((err, doc) => {
-        if (doc) {
-            this.findOneAndUpdate({"name": "zjson-vc"}, {$set: {count: count}}, callback);
-        } else {
-            this.create(new VisitCountModel({
-                "name": "zjson-vc", "count": count
-            }), callback);
-        }
-    });
+    this.findOne({"name": "zjson-vc"}, {}, callback);
 }
 
 module.exports = VisitCountModel;
