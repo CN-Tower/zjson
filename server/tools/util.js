dac982fca25229d082a650b45a3d72fa64f7ea30
CN-Tower <cntower@yahoo.com>
2018-08-24 15:43:36 +0800

    Update util.js

--- server/tools/util.js
+++ server/tools/util.js
@@ -4,9 +4,10 @@ module.exports = {
     logErr: logErr
 }
 
-function logErr(err, tit) {
+function logErr(err, tit, res) {
     if (err) {
         fn.log(err, tit || 'Error:');
+        if (res) res.status(400).send({'errorMsg': err.message});
     }
 }
 
