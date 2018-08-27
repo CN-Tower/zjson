a9a431ce2483a0579f7f29d3968315d0ec4b2814
CN-Tower <cntower@yahoo.com>
2018-08-27 17:21:20 +0800

    Update version.model.js

--- server/dao/models/version.model.js
+++ server/dao/models/version.model.js
@@ -10,4 +10,13 @@ VersionModel.setVersion = function(version, callback) {
     this.findOneAndUpdate({"name": "zjson-version"}, {$set: {version: version}}, callback);
 }
 
+VersionModel.getAppVersion = function(callback) {
+  this.findOne({"name": "zjson-version-app"}, callback);
+}
+
+VersionModel.setAppVersion = function(data, callback) {
+  data.name = 'zjson-version-app';
+  this.findOneAndUpdate({"name": "zjson-version-app"}, data, {"new": true}, callback);
+}
+
 module.exports = VersionModel;
