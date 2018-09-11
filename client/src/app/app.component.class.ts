79ae2c258e9a0cd4a8b456e21a7d97be2bfa7ea3
CN-Tower <cntower@yahoo.com>
2018-09-12 07:45:19 +0800

    Update app.component.class.ts

--- client/src/app/app.component.class.ts
+++ client/src/app/app.component.class.ts
@@ -1,8 +1,8 @@
 import { Configs, FmtStatus, FmterEles } from './formatter/formatter.conf';
 import { Formatter } from './formatter/formatter.core';
-import { APP_INFO } from './app.service';
+import { AppService, APP_INFO } from './app.service';
 
-export class ZjsApp {
+export abstract class ZjsApp {
   appUrl: string = APP_INFO.appUrl;
   conf: Configs;
   lang: string;
@@ -39,11 +39,34 @@ export class ZjsApp {
   alertInfo: any = {type: '', idx: NaN, brc: ''};
   alertType: 'info'|'success'|'warning'|'danger' = 'info';
   greeting: string;
+  srcEditor: any;
+  fmtEditor: any;
+  errRowIndex: number;
+  errRowDecorations: any[] = [];
   i18n: any = {
     confs: {show: '', hide: ''},
     model: {expand: '', combine: ''},
     alert: {ost: '', col: '', val: '', end: '', war: '', scc: ''}
   };
+  srcEditorOptions: any = {
+    language: 'text/plain',
+    tabSize: 2,
+    theme: this.appService.getEditorTheme(this.appService.getAppTheme()),
+    minimap: {
+      enabled: false
+    },
+    scrollbar: {
+      horizontal: 'hidden'
+    }
+  };
+  fmtEditorOptions: any = {
+    theme: this.appService.getEditorTheme(this.appService.getAppTheme()),
+    language: 'json',
+    tabSize: 2
+  };
+  getFmtHists = () => this.fmtHists = this.appService.getFmtHists();
   getTimeStr = () => fn.fmtDate('MM-dd hh:mm:ss');
   setRowIdxWpHeight = () => $('.z-canvas').height() + 12 + 'px';
+
+  constructor (public appService: AppService) { }
 }
