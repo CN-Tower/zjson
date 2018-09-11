ba5dade2834d106b0d30f996d2fa299babdb2377
CN-Tower <cntower@yahoo.com>
2018-09-12 07:50:47 +0800

    Update zjs-info.component.ts

--- client/src/app/attachments/zjs-info.component.ts
+++ client/src/app/attachments/zjs-info.component.ts
@@ -5,41 +5,44 @@ import { APP_INFO } from '../app.service';
 @Component({
   selector: 'zjs-info',
   template: `
-    <span class="z-sm-hide" (click)="openModal(infoTpl)">
+    <span class="z-ico-hd" (click)="openModal(infoTpl)">
       <i class="fa fa-info-circle z-op-icon" title="{{'versionInfo' | translate}}"></i>
       <ng-template #infoTpl>
-        <div class="modal-header">
-          <h4 class="modal-title pull-left">转杰森 | ZJSON</h4>
-          <button type="button" class="close pull-right" aria-label="Close" (click)="modalRef.hide()">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="z-info-modal modal-body">
-          <table>
-            <tr>
-              <td>{{'version' | translate}}:</td>
-              <td>{{version}}</td>
-            </tr>
-            <tr>
-              <td>{{'author' | translate}}:</td>
-              <td>@CN-Tower</td>
-            </tr>
-            <tr>
-              <td>{{'email' | translate}}:</td>
-              <td>cntower@yahoo.com</td>
-            </tr>
-            <tr>
-              <td>{{'qqGroup' | translate}}:</td>
-              <td>174136590</td>
-            </tr>
-            <tr>
-              <td>{{'updateTime' | translate}}:</td>
-              <td>{{updateTime}}</td>
-            </tr>
-          </table>
+        <div id="zjs-info">
+          <div class="modal-header">
+            <h4 class="modal-title pull-left">转杰森 | ZJSON</h4>
+            <button type="button" class="close pull-right" aria-label="Close" (click)="modalRef.hide()">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="z-info-modal modal-body">
+            <table>
+              <tr>
+                <td>{{'version' | translate}}:</td>
+                <td>{{version}}</td>
+              </tr>
+              <tr>
+                <td>{{'author' | translate}}:</td>
+                <td>@CN-Tower</td>
+              </tr>
+              <tr>
+                <td>{{'email' | translate}}:</td>
+                <td>cntower@yahoo.com</td>
+              </tr>
+              <tr>
+                <td>{{'qqGroup' | translate}}:</td>
+                <td>174136590</td>
+              </tr>
+              <tr>
+                <td>{{'updateTime' | translate}}:</td>
+                <td>{{updateTime}}</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </ng-template>
-    </span>`
+    </span>`,
+  styleUrls: ['./zjs-attachments.less']
 })
 export class ZjsInfoComponent {
   modalRef: BsModalRef;
