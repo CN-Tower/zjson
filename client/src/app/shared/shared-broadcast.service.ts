7904c29f1a94db6f5516e9d08f01beedf00b0f4e
CN-Tower <cntower@yahoo.com>
2018-09-15 15:42:37 +0800

    Update shared-broadcast.service.ts

--- client/src/app/shared/shared-broadcast.service.ts
+++ client/src/app/shared/shared-broadcast.service.ts
@@ -3,14 +3,23 @@ import { HintInfo } from './shared-interface';
 
 @Injectable()
 export class SharedBroadcastService {
-  hintEmiter: EventEmitter<any> = new EventEmitter;
-  editorEmiter: EventEmitter<any> = new EventEmitter;
+  private loadingEmiter: EventEmitter<boolean> = new EventEmitter;
+  private hintEmiter: EventEmitter<any> = new EventEmitter;
+  private editorEmiter: EventEmitter<any> = new EventEmitter;
+
+  loadingStream: any = this.loadingEmiter.asObservable();
   hintStream: any = this.hintEmiter.asObservable();
   editorStream: any = this.editorEmiter.asObservable();
 
   constructor() { }
 
-  showHint(hintInfo: HintInfo) {
-    this.hintEmiter.emit(hintInfo);
+  showLoading = (time?: number) => {
+    this.loadingEmiter.emit(true);
+    if (fn.typeVal(time, 'num') >= 0) {
+      fn.timeout('zjs-loading-timer', time, () => this.hideLoading());
+    }
   }
+  hideLoading = () => this.loadingEmiter.emit(false);
+  showHint = (hintInfo: HintInfo) => this.hintEmiter.emit(hintInfo);
+  editorReadyUp = () => this.editorEmiter.emit();
 }
