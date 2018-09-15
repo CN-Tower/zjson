62e77b3ce5c20cc40707450faff6ec10813a9681
CN-Tower <cntower@yahoo.com>
2018-09-15 15:35:56 +0800

    Update zjs-loading.component.ts

--- client/src/app/attachments/zjs-loading.component.ts
+++ client/src/app/attachments/zjs-loading.component.ts
@@ -1,9 +1,10 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { SharedBroadcastService } from '../shared/shared-broadcast.service';
 
 @Component({
   selector: 'zjs-loading',
   template: `
-    <div class="z-loading">
+    <div *ngIf="isShowLoading" class="z-loading">
       <div class="loadingBg"></div>
       <div class="loadingText top45">
         <div class="loadingGif">
@@ -14,6 +15,14 @@ import { Component} from '@angular/core';
       </div>
     </div>`
 })
-export class ZjsLoadingComponent {
-  constructor() { }
+export class ZjsLoadingComponent implements OnInit {
+  isShowLoading: boolean = false;
+
+  constructor(private broadcast: SharedBroadcastService) { }
+
+  ngOnInit() {
+    this.broadcast.loadingStream.subscribe(isShow => {
+      this.isShowLoading = isShow;
+    });
+  }
 }
