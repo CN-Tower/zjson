08b3f7cab050e982e7f6a8103ff7274d4a30c18b
CN-Tower <cntower@yahoo.com>
2018-09-15 15:42:55 +0800

    Update shared-interface.ts

--- client/src/app/shared/shared-interface.ts
+++ client/src/app/shared/shared-interface.ts
@@ -1,6 +1,10 @@
 export interface HintInfo {
   hintMsg: string;
-  hintType: 'danger'|'success';
+  hintType?: 'danger'|'success';
 }
-
 export type EditorModal = 'source' | 'format';
+export interface FmtHist {
+  name: string;
+  src: any;
+}
+export type DiffType = 'src' | 'new' | string;
