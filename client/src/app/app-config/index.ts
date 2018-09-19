b87dccbf26c201f8b026720348ed666015e14e98
CN-Tower <cntower@yahoo.com>
2018-09-19 13:55:30 +0800

    Update index.ts

--- client/src/app/app-config/index.ts
+++ client/src/app/app-config/index.ts
@@ -2,4 +2,4 @@ export * from './app-config.component';
 export * from './config-items/config-lang.component';
 export * from './config-items/config-theme.component';
 export * from './config-items/config-indent.component';
-export * from './config-items/config-type.component';
+export * from './config-items/config-quote.component';
