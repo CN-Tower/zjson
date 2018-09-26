138ceb1d460b72a686a5100cf867e01972fadd01
CN-Tower <cntower@yahoo.com>
2018-09-26 11:33:30 +0800

    Update formatter.conf.ts

--- client/src/app/formatter/formatter.conf.ts
+++ client/src/app/formatter/formatter.conf.ts
@@ -1,18 +1,19 @@
 import { FmtHelp } from './formatter.help';
+import { AlertType, AlertInfo, Indent, FmtedType } from '../shared';
 
 /**
  * 格式化配置
  * =======================================
  */
 export class Configs {
-  public model: 'expand' | 'combine' = 'expand';
-  public type: 'json' | 'jsObj' | 'pyMap' = 'json';
-  public indent: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 = 2;
-  public sgIndent: string = ' ';
-  public keyQuote: '\'' | '\"' | '';
-  public valQuote: '\'' | '\"';
-  public isStrict: boolean;
-  public isEscape: boolean;
+  model: 'expand' | 'combine' = 'expand';
+  type: FmtedType = 'json';
+  indent: Indent = 2;
+  sgIndent: string = ' ';
+  keyQuote: '\'' | '\"' | '';
+  valQuote: '\'' | '\"';
+  isStrict: boolean;
+  isEscape: boolean;
 }
 
 /**
@@ -20,14 +21,15 @@ export class Configs {
  * =======================================
  */
 export class FmtStatus {
-  public isSrcValid: boolean = true;
-  public fmtedRows: number = 0;
-  public errRowStr: number = 0;
-  public errRowIdx: number = 0;
-  public fmtedLines: number = 0;
-  public altType: 'info' | 'success' | 'warning' | 'danger' = 'info';
-  public altInfo: any = { type: '', idx: NaN, brc: '' };
-  public fmtedType: 'json' | 'jsObj' | 'pyMap' | 'other' | '' = 'json';
+  isSrcValid: boolean = true;
+  isFmtError: boolean = false;
+  fmtedRows: number = 0;
+  errRowStr: number = 0;
+  errRowIdx: number = 0;
+  fmtedLines: number = 0;
+  altType: AlertType = 'info';
+  altInfo: AlertInfo = { type: '', idx: NaN, brc: '' };
+  fmtedType: FmtedType = 'json';
 }
 
 /**
@@ -35,11 +37,11 @@ export class FmtStatus {
  * =======================================
  */
 export class FmtValidator {
-  public exceptVal: string = '';
-  public exceptType: string = '';
-  public deepIdxCon: string = '';
-  public isSrcJson: boolean = true;
-  public srcAcType: 'json' | 'jsObj' | 'pyMap' | 'other' | '' = '';
+  exceptVal: string = '';
+  exceptType: string = '';
+  deepIdxCon: string = '';
+  isSrcJson: boolean = true;
+  srcAcType: FmtedType = '';
 }
 
 /**
