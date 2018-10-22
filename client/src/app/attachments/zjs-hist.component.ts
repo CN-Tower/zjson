040cbc9bb6323bcef007b1106c79482b04b1b115
CN-Tower <cntower@yahoo.com>
2018-10-22 14:28:16 +0800

    Update zjs-hist.component.ts

--- client/src/app/attachments/zjs-hist.component.ts
+++ client/src/app/attachments/zjs-hist.component.ts
@@ -5,7 +5,7 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
   template: `
     <span  class="z-ico-hd dropdown" dropdown>
       <i dropdownToggle class="fa fa-database z-op-icon dropdown-toggle"></i>
-      <ul *dropdownMenu id="hist-ops" class="dropdown-menu">
+      <ul *dropdownMenu id="hist-ops" class="dropdown-menu" [class.dropdown-menu-right]="isMenuRight">
         <li *ngIf="fmtHists.length==0">
           <a href="javascript:;">
             <i class="fa fa-exclamation-circle"></i>&nbsp;&nbsp;{{'noHist' | translate}}
@@ -21,6 +21,7 @@ import { Component, Input, Output, EventEmitter} from '@angular/core';
 })
 export class ZjsHistComponent {
   @Input() fmtHists: any[] = [];
+  @Input() isMenuRight: boolean = false;
   @Output() showOrRmFmtHist: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
