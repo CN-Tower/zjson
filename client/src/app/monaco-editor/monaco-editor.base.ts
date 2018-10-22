d45c48dbff5d085a74299806e79ace4b68e93656
CN-Tower <cntower@yahoo.com>
2018-10-22 14:30:45 +0800

    Update monaco-editor.base.ts

--- client/src/app/monaco-editor/monaco-editor.base.ts
+++ client/src/app/monaco-editor/monaco-editor.base.ts
@@ -2,42 +2,37 @@ import { AfterViewInit } from '@angular/core';
 import { SharedBroadcastService } from '../shared';
 import { MONACO_THEMES } from './monaco-editor.themes';
 
-let isEditorLoaded: boolean = false;
-
 export class MonacoEditorBase implements AfterViewInit {
 
   constructor(public broadcast: SharedBroadcastService) {}
 
   ngAfterViewInit() {
-    if (isEditorLoaded) {
+    if (this.broadcast.isEditorLoaded) {
       const editor = fn.get(win, 'monaco/editor');
       if (editor) this.broadcast.editorReadyUp();
     } else {
-      isEditorLoaded = true;
-      this.addLoaderScript();
-    }
-  }
-
-  addLoaderScript() {
-    /**electron ignore sta*/
-    const loaderScript: HTMLScriptElement = document.createElement('script');
-    loaderScript.type = 'text/javascript';
-    loaderScript.src = 'assets/lib/monaco-editor/vs/loader.js';
-    loaderScript.addEventListener('load', () => {
-      win.require.config({paths: {'vs': 'assets/lib/monaco-editor/vs'}});
-      win.require(['vs/editor/editor.main'], () => {
+      /**electron ignore sta*/
+      const loaderScript: HTMLScriptElement = document.createElement('script');
+      loaderScript.type = 'text/javascript';
+      loaderScript.src = 'assets/lib/monaco-editor/vs/loader.js';
+      loaderScript.addEventListener('load', () => {
+        win.require.config({paths: {'vs': 'assets/lib/monaco-editor/vs'}});
+        win.require(['vs/editor/editor.main'], () => {
+          this.defineEditorThemes();
+          this.broadcast.editorReadyUp();
+          this.broadcast.isEditorLoaded = true;
+        });
+      });
+      document.body.appendChild(loaderScript);
+      /**electron ignore end*/
+      /**electron enable sta_*//*
+      fn.defer(() => win.loadMonacoEditor(() => {
         this.defineEditorThemes();
         this.broadcast.editorReadyUp();
-      });
-    });
-    document.body.appendChild(loaderScript);
-    /**electron ignore end*/
-    /**electron enable sta_*//*
-    fn.defer(() => win.loadMonacoEditor(() => {
-      this.defineEditorThemes();
-      this.broadcast.editorReadyUp();
-    }));
-    *//**electron enable end_*/
+        this.broadcast.isEditorLoaded = true;
+      }));
+      *//**electron enable end_*/
+    }
   }
 
   defineEditorThemes() {
