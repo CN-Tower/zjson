f865ccb40aa8b9fbd1c5707c8a4ab90d52cbe951
CN-Tower <cntower@yahoo.com>
2018-11-22 13:32:11 +0800

    Update sharedJson.model.js

--- server/models/sharedJson.model.js
+++ server/models/sharedJson.model.js
@@ -1,29 +1,39 @@
 const mongoose = require('mongoose');
-const SharedJsonSchema = require('../schemas/sharedJson.schema');
+
+const SharedJsonSchema = new mongoose.Schema({
+  userId: String,
+  sharedJson: String,
+  updateTime: Number,
+}, {
+  collection: 'sharedJson'
+});
+
 const SharedJsonModel = mongoose.model('shared-json', SharedJsonSchema);
 
-SharedJsonModel.getSjList = function(userId, callback) {
+SharedJsonModel.getSjList = function (userId, callback) {
   this.find({}, callback);
 }
 
-SharedJsonModel.getSjsByUserId = function(userId, callback) {
-  this.find({"userId": userId}, callback);
+SharedJsonModel.getSjsByUserId = function (userId, callback) {
+  this.find({ "userId": userId }, callback);
 }
 
-SharedJsonModel.getSjByUserId = function(userId, callback) {
-  this.findOne({"userId": userId}, callback);
+SharedJsonModel.getSjByUserId = function (userId, callback) {
+  this.findOne({ "userId": userId }, callback);
 }
 
-SharedJsonModel.createSj = function(sharedJson, callback) {
+SharedJsonModel.createSj = function (sharedJson, callback) {
   this.create(new SharedJsonModel(sharedJson), callback);
 }
 
-SharedJsonModel.removeSjById = function(userId, callback) {
-  this.remove({"userId": userId}, callback);
+SharedJsonModel.removeSjById = function (userId, callback) {
+  this.remove({ "userId": userId }, callback);
 }
 
-SharedJsonModel.updateSjById = function(userId, data, callback) {
-  this.findOneAndUpdate({"userId": userId}, data, {"new": true}, callback);
+SharedJsonModel.updateSjById = function (userId, data, callback) {
+  this.findOneAndUpdate(
+    { "userId": userId }, data, { "new": true }, callback
+  );
 }
 
 module.exports = SharedJsonModel;
