8e9e2390420b033a20177e2ecb9be0e276663957
CN-Tower <cntower@yahoo.com>
2018-11-22 13:31:43 +0800

    Delete version.schema.js

--- server/dao/schemas/version.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const VersionSchema = new mongoose.Schema({
-    name: String,
-    version: String,
-    updateUrl: String,
-}, {collection: 'version'});
-
-module.exports = VersionSchema;
