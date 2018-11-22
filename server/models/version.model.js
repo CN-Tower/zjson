3ee37474d5f8c5d08eab77d6b277e2dabf5b41b1
CN-Tower <cntower@yahoo.com>
2018-11-22 13:32:51 +0800

    Update version.model.js

--- server/models/version.model.js
+++ server/models/version.model.js
@@ -1,22 +1,36 @@
 const mongoose = require('mongoose');
-const VersionSchema = require('../schemas/version.schema');
+
+const VersionSchema = new mongoose.Schema({
+    name: String,
+    version: String,
+    updateUrl: String,
+}, {
+  collection: 'version'
+});
+
 const VersionModel = mongoose.model('zjson-version', VersionSchema);
 
 VersionModel.getVersion = function(callback) {
-    this.findOne({"name": "zjson-version"}, callback);
+  this.findOne({"name": "zjson-version"}, callback);
 }
 
 VersionModel.setVersion = function(version, callback) {
-    this.findOneAndUpdate({"name": "zjson-version"}, {$set: {version: version}}, callback);
+  this.findOneAndUpdate(
+    {"name": "zjson-version"}, {$set: {version: version}}, callback
+  );
 }
 
 VersionModel.getAppVersion = function(callback) {
-  this.findOne({"name": "zjson-version-app"}, callback);
+  this.findOne(
+    {"name": "zjson-version-app"}, callback
+  );
 }
 
 VersionModel.setAppVersion = function(data, callback) {
   data.name = 'zjson-version-app';
-  this.findOneAndUpdate({"name": "zjson-version-app"}, data, {"new": true}, callback);
+  this.findOneAndUpdate(
+    {"name": "zjson-version-app"}, data, {"new": true}, callback
+  );
 }
 
 module.exports = VersionModel;
