1d51e0f3049efff1fdddcb3c0f396dd4b123d73a
CN-Tower <cntower@yahoo.com>
2018-11-22 13:32:30 +0800

    Update users.model.js

--- server/models/users.model.js
+++ server/models/users.model.js
@@ -1,33 +1,45 @@
 const mongoose = require('mongoose');
-const UserSchema = require('../schemas/users.schema');
+
+const UserSchema = new mongoose.Schema({
+  'userId': String,
+  'vtTime': Number,
+  'isActive': Boolean,
+  'isKeepAc': Boolean
+}, {
+  timestamps: true,
+  collection: 'users'
+});
+
 const UsersModel = mongoose.model('zjson-users', UserSchema);
 
-UsersModel.getUsers = function(callback) {
-    this.find().exec(callback);
+UsersModel.getUsers = function (callback) {
+  this.find().exec(callback);
 }
 
-UsersModel.getOneUserById = function(userId, callback) {
-  this.findOne({userId: userId}, callback);
+UsersModel.getOneUserById = function (userId, callback) {
+  this.findOne({ userId: userId }, callback);
 }
 
-UsersModel.getUsersById = function(userId, callback) {
-    this.find({userId: userId}, callback);
+UsersModel.getUsersById = function (userId, callback) {
+  this.find({ userId: userId }, callback);
 }
 
-UsersModel.getOnline = function(callback) {
-    this.count().exec(callback);
+UsersModel.getOnline = function (callback) {
+  this.count().exec(callback);
 }
 
-UsersModel.createUser = function(userInfo, callback) {
-    this.create(new UsersModel(userInfo), callback);
+UsersModel.createUser = function (userInfo, callback) {
+  this.create(new UsersModel(userInfo), callback);
 }
 
-UsersModel.removeUser = function(userId, callback) {
-    this.remove({"userId": userId}, callback);
+UsersModel.removeUser = function (userId, callback) {
+  this.remove({ "userId": userId }, callback);
 }
 
-UsersModel.updateUser = function(userId, callback) {
-    this.findOneAndUpdate({"userId": userId}, {$set: {'isKeepAc': true}}, callback);
+UsersModel.updateUser = function (userId, callback) {
+  this.findOneAndUpdate(
+    { "userId": userId }, { $set: { 'isKeepAc': true } }, callback
+  );
 }
 
 module.exports = UsersModel;
