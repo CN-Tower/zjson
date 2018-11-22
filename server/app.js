f8c467319fe9885f34207137e6ff9a2f42f3c212
CN-Tower <cntower@yahoo.com>
2018-11-22 16:44:00 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,62 +1,61 @@
-var express = require('express');
-var path = require('path');
-var fs = require('fs');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var btService = require('./service/base-thread.service');
+const express = require('express');
+const fn = require('funclib');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const timeout = require('connect-timeout');
+const mongoose = require('mongoose');
+const routers = require('./routes');
+const config = require('./config');
 
-var index = require('./routes/index');
-var api = require('./routes/api');
+require('./service/taskThread.service')();
 
-btService.start();
+mongoose.connect(config.dburl);
 
-// connect to mongodb
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1/zjson');
-// mongoose.connect('mongodb://user:pwd@127.0.0.1/zjson');
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'unable to connect to mongodb'));
-db.once('open', function() {
-  console.log("connect to mongodb success...");
+const db = mongoose.connection;
+db.on('error', () => {
+  fn.log('数据库连接失败！', { title: 'MongoDB', color: 'red' });
+});
+db.once('open', () => {
+  fn.log('数据库连接成功！', { title: 'MongoDB', color: 'green' });
 });
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.all('*', (req, res, next) => {
+  res.header("X-Powered-By", '1.0.1');
+  if (req.method === 'OPTIONS') {
+    res.status(200).send({ status: 1, message: 'ok' });
+  } else {
+    next();
+  }
+});
 
-// uncomment after placing your favicon in /zjson
-//app.use(favicon(path.join(__dirname, 'zjson', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'zjson')));
+app.use(timeout(10000));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
 
-app.use('/', index);
-app.use('/api', api);
+app.use('/api', routers);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((err, req, res, next) => {
+  res.status(err.status || fn.typeVal(err.code, 'num') || 500);
+  res.json({ status: 0, message:  err.message });
 });
 
-module.exports = app;
+app.listen(config.port, () => {
+  fn.log(`Listenig on port: ${config.port}`, { title: 'ZJSON.NET', color: 'green' });
+});
+
