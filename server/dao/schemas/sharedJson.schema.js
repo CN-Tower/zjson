def3bfa9fe3efea99f18773e34f845f3616ca563
CN-Tower <cntower@yahoo.com>
2018-11-22 13:31:30 +0800

    Delete sharedJson.schema.js

--- server/dao/schemas/sharedJson.schema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const SharedJsonSchema = new mongoose.Schema({
-    userId: String,
-    sharedJson: String,
-    updateTime: Number,
-}, {collection: 'sharedJson'});
-
-module.exports = SharedJsonSchema;
