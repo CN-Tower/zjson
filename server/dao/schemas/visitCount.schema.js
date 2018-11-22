562020f2354aaa4ee9367fe30127f50702f89805
CN-Tower <cntower@yahoo.com>
2018-11-22 13:31:53 +0800

    Delete visitCount.schema.js

--- server/dao/schemas/visitCount.schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const VisitCountSchema = new mongoose.Schema({
-    name: String,
-    count: Number,
-}, {collection: 'visitCount'});
-
-module.exports = VisitCountSchema;
