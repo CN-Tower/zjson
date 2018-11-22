6ed019e97cd42103e8f9be50536dff064e974de6
CN-Tower <cntower@yahoo.com>
2018-11-22 13:21:41 +0800

    Update monaco-editor.base.ts

--- client/src/app/monaco-editor/monaco-editor.base.ts
+++ client/src/app/monaco-editor/monaco-editor.base.ts
@@ -14,9 +14,26 @@ export class MonacoEditorBase implements AfterViewInit {
       /**==================== electron ignore sta ====================*/
       const loaderScript: HTMLScriptElement = document.createElement('script');
       loaderScript.type = 'text/javascript';
+      /**-------------------- local monaco editor ---------------------
       loaderScript.src = 'assets/lib/monaco-editor/vs/loader.js';
       loaderScript.addEventListener('load', () => {
         win.require.config({paths: {'vs': 'assets/lib/monaco-editor/vs'}});
+      ----------------------------------------------------------------*/
+      /**-------------------- cdnjs monaco editor --------------------*/
+      loaderScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.15.0/min/vs/loader.js';
+      loaderScript.addEventListener('load', () => {
+        win.require.config({paths: {'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.15.0/min/vs'}});
+        win.MonacoEnvironment = {
+          getWorkerUrl: function(workerId, label) {
+            return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
+              self.MonacoEnvironment = {
+                baseUrl: 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.15.0/min/'
+              };
+              importScripts('https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.15.0/min/vs/base/worker/workerMain.js');`
+            )}`;
+          }
+        };
+      /**-------------------------------------------------------------*/
         win.require(['vs/editor/editor.main'], () => {
           this.defineEditorThemes();
           this.broadcast.editorReadyUp();
