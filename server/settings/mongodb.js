71f49cf4a73ec91397bf5a077d9e7e3fc5042fa7
CN-Tower <cntower@yahoo.com>
2018-11-23 16:24:29 +0800

    Create mongodb.js

---
+++ server/settings/mongodb.js
@@ -0,0 +1,26 @@
+const fn = require('funclib');
+const mongoose = require('mongoose');
+const config = require('../config');
+
+module.exports = function () {
+
+  mongoose.Promise = global.Promise;
+  const db = mongoose.connection;
+
+  db.on('connected', function() {
+    fn.log('Connect to MongoDB success!', { title: 'MongoDB', color: 'green' });
+  });
+
+  db.on('error', err => {
+    fn.log(`Connect to MongoDB failed: ${err}`, { title: 'MongoDB', color: 'red' });
+    mongoose.disconnect();
+  });
+
+  db.on('disconnected', () => {
+    fn.log('Reconnecting to MongoDB...', { title: 'MongoDB', color: 'green' });
+    mongoose.connect(config.dbUrl, { useNewUrlParser: true });
+  });
+
+  mongoose.connect(config.dbUrl, { useNewUrlParser: true }); 
+
+}
