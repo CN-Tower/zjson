753907d97b549f195edc027c512b87e734fd99a3
CN-Tower <cntower@yahoo.com>
2018-11-23 16:27:52 +0800

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,51 +1,26 @@
-const express  = require('express');
-const path     = require('path');
-const fn       = require('funclib');
-const logger   = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser   = require('body-parser');
-const timeout  = require('connect-timeout');
-const routers  = require('./routes');
-const config   = require('./config');
+const fn = require('funclib');
+const mongoose = require('mongoose');
+const config = require('../config');
 
-require('./settings/mongodb')();
-require('./service/taskThread.service')();
+module.exports = function () {
 
-const app = express();
+  mongoose.Promise = global.Promise;
+  const db = mongoose.connection;
 
-app.all('*', (req, res, next) => {
-  res.header("X-Powered-By", '1.0.1');
-  if (req.method === 'OPTIONS') {
-    res.status(200).send({ status: 1, message: 'ok' });
-  } else {
-    next();
-  }
-});
+  db.on('connected', function() {
+    fn.log('Connect to MongoDB success!', { title: 'MongoDB', color: 'green' });
+  });
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'zjson')));
-app.use(timeout(10000));
-app.use((req, res, next) => {
-  if (!req.timedout) next();
-});
+  db.on('error', err => {
+    fn.log(`Connect to MongoDB failed: ${err}`, { title: 'MongoDB', color: 'red' });
+    mongoose.disconnect();
+  });
 
-app.use('/api', routers);
+  db.on('disconnected', () => {
+    fn.log('Reconnecting to MongoDB...', { title: 'MongoDB', color: 'green' });
+    fn.timeout(5000, () => mongoose.connect(config.dbUrl, { useNewUrlParser: true }));
+  });
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  mongoose.connect(config.dbUrl, { useNewUrlParser: true }); 
 
-app.use((err, req, res, next) => {
-  res.status(err.status || fn.typeVal(err.code, 'num') || 500);
-  res.json({ status: 0, message:  err.message });
-});
-
-app.listen(config.port, () => {
-  fn.log(`Server is Listenig on port: ${config.port}`, { title: 'ZJSON.NET', color: 'green' });
-});
-
+}
