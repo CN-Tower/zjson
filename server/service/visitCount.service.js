4e5684cb26091ff838296f00cfa900a1d4b621d5
CN-Tower <cntower@yahoo.com>
2018-11-23 20:30:46 +0800

    Update visitCount.service.js

--- server/service/visitCount.service.js
+++ server/service/visitCount.service.js
@@ -5,49 +5,81 @@ const UsersModel = require('../models/users.model');
 /**
  * 轮询访问量
  * ------------------------------------------------------------*/
-exports.pollingVc = function (userId, isOnInit, next, callback) {
-  UsersModel.getUsersById(userId, (err, docs) => {
-    if (err) return next(err);
-    if (docs.length) {
-      if (docs.length === 1) {
-        setUserExpireTime(userId);
-      } else {
-        UsersModel.removeUser(userId, err => {
-          if (err) fn.log(fn.get(err, 'message'), 'Remove User Err');
-          addLoseUser(userId);
-        });
-      }
-    } else if (isOnInit === 'no' && /^ZJSON-[0-9A-Z]{12}$/.test(userId)) {
-      addLoseUser(userId);
-    }
-    VisitCountModel.getVisitCount((err, doc) => {
+exports.pollingVc = function (req, res, next) {
+  try {
+    const userId = req.params['userId'];
+    const isOnInit = req.query['isOnInit'];
+    UsersModel.getUsersById(userId, (err, docs) => {
+      if (req.timedout) return;
       if (err) return next(err);
-      callback(doc && doc.count || 0);
+      if (docs.length) {
+        if (docs.length === 1) {
+          setUserExpireTime(userId);
+        } else {
+          UsersModel.removeUser(userId, err => {
+            if (err) fn.log(fn.get(err, 'message'), 'Remove User Err');
+            addLoseUser(userId);
+          });
+        }
+      } else if (isOnInit === 'no' && /^ZJSON-[0-9A-Z]{12}$/.test(userId)) {
+        addLoseUser(userId);
+      }
+      VisitCountModel.getVisitCount((err, doc) => {
+        if (req.timedout) return;
+        if (err) return next(err);
+        res.status(200).send({'vc': doc && doc.count || 0});
+      });
     });
-  });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 /**
  * 刷新访问量
  * ------------------------------------------------------------*/
-exports.refreshVc = function (userId, isExpire, next, callback) {
-  UsersModel.getOneUserById(userId, (err, doc) => {
-    if (err) return next(err);
-    if (doc) {
-      if (isExpire === 'yes' && doc.isKeepAc) {
-        VisitCountModel.addOneVisit((err, doc) => {
+exports.refreshVc = function (req, res, next) {
+  try {
+    const userId = req.params['userId'];
+    const isExpire = req.query['isExpire'];
+    UsersModel.getOneUserById(userId, (err, doc) => {
+      if (req.timedout) return;
+      if (err) return next(err);
+      if (doc) {
+        if (isExpire === 'yes' && doc.isKeepAc) {
+          VisitCountModel.addOneVisit(err => {
+            if (err) fn.log(fn.get(err, 'message'), 'Add One Visit Err');
+          });
+        }
+        res.status(200).send({'id': null});
+      } else {
+        VisitCountModel.addOneVisit(err => {
           if (err) fn.log(fn.get(err, 'message'), 'Add One Visit Err');
         });
+        userId = 'ZJSON-' + fn.rdid();
+        const vtTime = fn.time();
+        const newUser = {
+          'userId': userId,
+          'vtTime': vtTime,
+          'isActive': true,
+          'isKeepAc': false
+        };
+        UsersModel.createUser(newUser, err => {
+          if (req.timedout) return;
+          if (err) return next(err);
+          res.status(200).send({'id': userId});
+        });
+        setUserExpireTime(userId);
+        fn.timeout(5 * 60 * 1000, () => {
+          UsersModel.updateUser(userId, err => {
+            if (err) fn.log(fn.get(err, 'message'), 'Update User Err');
+          });
+        });
       }
-      callback(null);
-    } else {
-      VisitCountModel.addOneVisit((err, doc) => {
-        if (err) fn.log(fn.get(err, 'message'), 'Add One Visit Err');
-      });
-      userId = 'ZJSON-' + fn.rdid();
-      addUser(userId, next, () => callback(userId));
-    }
-  });
+    });
+  } catch (err) {
+    return next(err);
+  }
 }
 
 function setUserExpireTime(userId) {
@@ -66,24 +98,4 @@ function addLoseUser(userId) {
     'isKeepAc': true
   });
   setUserExpireTime(userId);
-}
-
-function addUser(userId, next, callback) {
-  const vtTime = fn.time();
-  const newUser = {
-    'userId': userId,
-    'vtTime': vtTime,
-    'isActive': true,
-    'isKeepAc': false
-  };
-  UsersModel.createUser(newUser, err => {
-    if (err) return next(err);
-    callback();
-  });
-  setUserExpireTime(userId);
-  fn.timeout(310000, () => {
-    UsersModel.updateUser(userId, err => {
-      if (err) fn.log(fn.get(err, 'message'), 'Update User Err');
-    });
-  });
 }
