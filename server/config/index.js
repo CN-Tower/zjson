d10ae784b39a3aac113faecc72d92639a75793fd
CN-Tower <cntower@yahoo.com>
2018-11-23 16:23:29 +0800

    Update index.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,11 +1,11 @@
-const dburls = {
+const dbUrls = {
   0: 'mongodb://127.0.0.1/zjson',
   1: 'mongodb://user:pass@dbhost/zjson',
   2: 'mongodb://10.40.154.118/zjson',
 }
 
 module.exports = {
-  port: 3300,
-  dburl: dburls[ 0 ],
+  port: 3000,
+  dbUrl: dbUrls[ 0 ],
   sharedJsonExp: 2 * 24 * 60 * 60 * 1000
 }
